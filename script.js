d0a59eceda8fcf2893d604b866fdc4b81eb4b5dd
vkadam1014 <virajk063@gmail.com>
2025-08-01 22:11:51 -0400

    New updates

--- script.js
+++ script.js
@@ -12,24 +12,28 @@ var svg = d3.select('body').append('svg')
 
 let stock_data;
 
-//Retrieve data from csv file
-async function getData(){
-    const data = await d3.csv("US_Stock_Data.csv", d => {
-        d.Date = new Date(d.Date);
-        for (i in d){
-            if (i !== "Date"){
-                d[i] = +d[i];
-            }
-        }
-        return d;
-    })
-}
-
-
-
 const stocks = ["S&P_500_Price", "Tesla_Price", "Apple_Price"];
 const oil = ["Natural_Gas_Price", "Crude_Oil_Price"];
 const commodities = ["Copper_Price", "Platinum_Price", "Silver_Price", "Gold_Price"];
+
+//Retrieve data from csv file
+async function getData(){
+    const data = await d3.csv("US_Stock_Data.csv", d => {
+       d.Date = new Date(d.Date);
+       d["S&P_500_Price"] = +d["S&P_500_Price"];
+       d["Tesla_Price"] = +d["Tesla_Price"];
+       d["Apple_Price"] = +d["Apple_Price"];
+       d["Natural_Gas_Price"] = +d["Natural_Gas_Price"];
+       d["Crude_Oil_Price"] = +d["Crude_Oil_Price"];
+       d["Copper_Price"] = d+["Copper_Price"];
+       d["Platinum_Price"] = d+["Platinum_Price"];
+       d["Silver_Price"]  = d+["Silver_Price"];
+       d["Gold_Price"]  = d+["Gold_Price"];
+       return d;
+    })
+    return data;
+}
+
 
 function createCharts(input){
     svg.selectAll("*").remove();
@@ -45,11 +49,11 @@ function createCharts(input){
     const yAxis = d3.axisLeft(y);
     const xAxis = d3.axisBottom(x);
 
-    svg.append("a")
+    svg.append("g")
         .attr("transform", `translate(0, ${height - margin.bottom})`)
         .call(xAxis);
     
-    svg.append("a")
+    svg.append("g")
         .attr("transform", `translate(${margin.left}, 0)`)
         .call(yAxis);
     input.forEach((i, j) => {
@@ -65,8 +69,9 @@ function createCharts(input){
         
         svg.append("text")
             .attr("x", width - 120)
-            .attr("y", 20 + i * 20)
+            .attr("y", 20 + j * 20)
             .text(i.replace("_Price", ""))
             .attr("fill", color(j));
     });
 }
+
